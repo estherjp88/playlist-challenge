3afd2836a65b68700090d5d7b52be8933296f740
estherjp88 <starburst8808@gmail.com>
2026-09-22 13:33:42 -0500

    Added new song

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Nicole Kidman", "Adela"),
 ]
 
 def print_playlist(songs):
